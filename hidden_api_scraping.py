500df17f5246d81c6f915434599ea771c9c12643
Saad Sameer Khan <127726151+SAAD3XK@users.noreply.github.com>
2023-08-09 20:58:53 +0500

    Add functionality for parsing product descriptions, use try-except block in parse_back method, remove logging

--- hidden_api_scraping.py
+++ hidden_api_scraping.py
@@ -1,10 +1,11 @@
+import json
 import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.http import Request
 import logging
 
 class On_Running_Crawler(scrapy.Spider):
-    logging.basicConfig(level=logging.INFO)
+    # logging.basicConfig(level=logging.INFO)
     name = "on_running_crawler"
     headers = {
             'authority': 'algolia.on-running.com',
@@ -45,16 +46,16 @@ class On_Running_Crawler(scrapy.Spider):
         totalPages=int(response_data.get('results')[0].get('nbPages'))
         current_page=int(response_data.get('results')[0].get('page'))
 
-        logging.info(f"Page number: {current_page}")
+        print(f"Page number: {current_page}")
         hits = response_data['results'][0]['hits']
         for item in hits:
             name = item['name']
             price = item['stores']['ch']['price']
             skus = item['stores']['ch']['purchasableSkus']
-            prod_url = 'https://www.on-running.com/en-ch' + item['productUrl']
+            prod_url = item['productUrl']
             last_sync = item['lastStockSynced']
             stock = item['stock']
-
+            
             result={}
             result['Product_name'] = name
             result['Product_url'] = prod_url
@@ -62,7 +63,46 @@ class On_Running_Crawler(scrapy.Spider):
             result['Product_price'] = price
             result['Product_skus'] = skus
             result['Product_last_sync'] = last_sync
-            yield result
+
+            short_page_url = prod_url.split('/')[2]
+
+            page_headers = {
+                'authority': 'on-graphql-gateway.on-running.com',
+                'accept': '*/*',
+                'accept-language': 'en-US,en;q=0.9',
+                'content-type': 'application/json',
+                'locale': 'en-ch',
+                'origin': 'https://www.on-running.com',
+                'original-url': f'https:://www.on-running.com/en-ch{prod_url}',
+                'referer': 'https://www.on-running.com/',
+                'sec-ch-ua': '"Not/A)Brand";v="99", "Microsoft Edge";v="115", "Chromium";v="115"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-site',
+                'session-id': '2d38451f-e7bf-4541-9042-368f667de796',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188',
+                'uuid': '1ebcb381-668c-4f18-91ad-381068ecea7d',
+            }
+
+            json_data = {
+                'operationName': 'page',
+                'variables': {
+                    'slug': f'products/{short_page_url}',
+                    'locale': 'en-US',
+                    'preview': False,
+                },
+                'extensions': {
+                    'persistedQuery': {
+                        'version': 1,
+                        'sha256Hash': '0e6561a67e88691c998567aaad155734a5eb60b3e37a6b97df43401fbd09a2e2',
+                    },
+                },
+            }
+            json_data_str = json.dumps(json_data)
+            
+            yield Request('https://on-graphql-gateway.on-running.com/', callback=self.parse_back, method="POST", body=json_data_str, meta={'result': result, 'prod_url':prod_url}, headers=page_headers)
 
         start_page = current_page + 1
 
@@ -70,6 +110,22 @@ class On_Running_Crawler(scrapy.Spider):
             data = '{"requests":[{"indexName":"CH_products_production_v3","params":"clickAnalytics=true&distinct=true&facets=%5B%22productType%22%2C%22productSubtype%22%2C%22genderFilter%22%2C%22family%22%2C%22activities%22%2C%22collections%22%2C%22cushioning%22%2C%22roadRunningStyle%22%2C%22conditions%22%2C%22surface%22%2C%22fit%22%2C%22colorCodes%22%2C%22lacing%22%2C%22features%22%2C%22tags%22%2C%22technology%22%2C%22terrain%22%2C%22productSubtypeStyle%22%5D&filters=NOT%20productUrl%3ANULL%20AND%20NOT%20imageUrl%3ANULL%20AND%20NOT%20groupingKey%3ANULL%20AND%20stores.ch.canBePurchased%3Atrue%20AND%20stores.ch.isHiddenFromSearch%3Afalse%20AND%20NOT%20tags%3Aexclude_from_plp%20AND%20NOT%20tags%3Aclassics%20AND%20NOT%20tags%3Alost_and_found&highlightPostTag=__%2Fais-highlight__&highlightPreTag=__ais-highlight__&maxValuesPerFacet=50&page='+ str(start_page)+ '&tagFilters=&userToken=xxxxxxxx"}]}'
             yield Request(self.main_url, callback=self.parse, method="POST", body=data, headers=self.headers)
 
+    def parse_back(self, response):
+        response_data = response.json()
+        try:
+            product_details = response_data['data']['pageCollection']['items'][0]['seo']
+            desc = product_details['description']
+
+            result = response.meta['result']
+            result['Product_description'] = desc
+
+            yield result
+        except:
+            print("--------------------------------------------------------")
+            print(f"Error!!! in {response.meta['prod_url']}")
+            print(response_data)
+            print("--------------------------------------------------------")
+
 process = CrawlerProcess()
 process.crawl(On_Running_Crawler)
-process.start()    
+process.start()
